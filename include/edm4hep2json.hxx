d97f6bf4c33f96af167766e6dc2ae2b95e764592
Juraj Smiesko <juraj.smiesko@cern.ch>
2022-07-01 15:58:17 +0200

    Converting Jets/Clusters

--- include/edm4hep2json.hxx
+++ include/edm4hep2json.hxx
@@ -1,10 +1,7 @@
 #ifndef EDM4HEP_TO_JSON_H__
 #define EDM4HEP_TO_JSON_H__
 
-// test data model
-#include "edm4hep/MCParticleCollection.h"
-#include "edm4hep/SimTrackerHitCollection.h"
-#include "edm4hep/CaloHitContributionCollection.h"
+// event data model
 #include "edm4hep/SimCalorimeterHitCollection.h"
 
 // podio specific includes
@@ -16,153 +13,70 @@
 // STL
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include <exception>
 #include <cassert>
 
-void processEvent(podio::EventStore& store, bool verboser, unsigned eventNum) {
-  auto& mcps   = store.get<edm4hep::MCParticleCollection>("MCParticles");
-  auto& sths   = store.get<edm4hep::SimTrackerHitCollection>("SimTrackerHits");
-  auto& schs   = store.get<edm4hep::SimCalorimeterHitCollection>("SimCalorimeterHits");
-  auto& sccons = store.get<edm4hep::CaloHitContributionCollection>("SimCalorimeterHitContributions");
+nlohmann::json processEvent(podio::EventStore& store,
+                            bool verboser,
+                            unsigned eventNum) {
+  auto& clusterCollection = store.get<edm4hep::SimCalorimeterHitCollection>(
+      "CorrectedCaloClusters");
 
-  nlohmann::json j;
+  nlohmann::json eventDict;
+  eventDict["event number"] = eventNum;
+  // eventDict["run number"] = 666;
+  eventDict["run number"] = 0;
+  nlohmann::json allClustersArray;
 
-  if( mcps.isValid() ){
+  if(clusterCollection.isValid()) {
 
-    // check that we can retrieve the correct information written in write_event.cpp :
-    //
-    //     name status pdg_id  parent Px       Py    Pz       Energy      Mass
-    //  1  !p+!    3   2212    0,0    0.000    0.000 7000.000 7000.000    0.938
-    //  2  !p+!    3   2212    0,0    0.000    0.000-7000.000 7000.000    0.938
-    //=========================================================================
-    //  3  !d!     3      1    1,1    0.750   -1.569   32.191   32.238    0.000
-    //  4  !u~!    3     -2    2,2   -3.047  -19.000  -54.629   57.920    0.000
-    //  5  !W-!    3    -24    1,2    1.517   -20.68  -20.605   85.925   80.799
-    //  6  !gamma! 1     22    1,2   -3.813    0.113   -1.833    4.233    0.000
-    //  7  !d!     1      1    5,5   -2.445   28.816    6.082   29.552    0.010
-    //  8  !u~!    1     -2    5,5    3.962  -49.498  -26.687   56.373    0.006
-
-    //-------- print particles for debugging:
-
-    std::cout << "\n collection:  " << "MCParticles" <<  " of type " <<  mcps.getValueTypeName() << "\n\n"
-        << mcps << std::endl ;
-    //-------------------------------
-
-    // check a few things (to be completed ...)
-    auto mcp1 = mcps[0] ;
-    if( mcp1.getPDG() != 2212 ) throw std::runtime_error("wrong PDG for first particle - should be 2212 " );
-
-    auto d0 = mcp1.getDaughters(0) ;
-    auto d1 = mcp1.getDaughters(1) ;
-    auto d2 = mcp1.getDaughters(2) ;
-
-    if( ! ( d0 == mcps[2] ) )  throw std::runtime_error(" error: 1. daughter of particle 0 is not particle 2 ");
-    if( ! ( d1 == mcps[4] ) )  throw std::runtime_error(" error: 2. daughter of particle 0 is not particle 4 ");
-    if( ! ( d2 == mcps[5] ) )  throw std::runtime_error(" error: 3. daughter of particle 0 is not particle 5 ");
+    /*
+    std::cout << "Collection: " << "CalorimeterHitCollection" <<  " of type "
+        <<  clusterCollection.getValueTypeName() << "\n\n"
+        << clusterCollection << std::endl;
+    */
 
 
-    auto mcp2 = mcps[1] ;
-    if( mcp2.getGeneratorStatus() != 3 ) throw std::runtime_error("wrong genStat for 2. particle - should be 3" );
-    // and so on ...
+    for (size_t i = 0;  i < clusterCollection.size(); ++i) {
+      auto cluster = clusterCollection[i];
+      auto clusterPosition = cluster.getPosition();
+      std::cout << "Event: " << eventNum <<  std::endl;
+      std::cout << "x: " << clusterPosition.x << std::endl;
+      std::cout << "y: " << clusterPosition.y << std::endl;
+      std::cout << "z: " << clusterPosition.z << std::endl;
+      std::cout << "e: " << cluster.getEnergy() << std::endl;
+
+      float pt = std::sqrt(std::pow(clusterPosition.x, 2) +
+                           std::pow(clusterPosition.y, 2));
+
+      float eta = std::asinh(clusterPosition.z/pt);
+      float phi = std::acos(clusterPosition.x/pt);
+      if (clusterPosition.y < 0.) {
+        phi *= -1;
+      }
+      float energy = cluster.getEnergy()*5000;
+      if (energy < 0.) {
+        energy *= -1;
+      }
+
+      nlohmann::json clusterDict;
+      clusterDict["phi"] = phi;
+      clusterDict["eta"] = eta;
+      clusterDict["energy"]= energy;
+
+      allClustersArray.emplace_back(clusterDict);
+    }
 
   } else {
     throw std::runtime_error("Collection 'MCParticles' should be present");
   }
 
-  //===============================================================================
-  if( sths.isValid() ){
+  nlohmann::json allJetsDict;
+  allJetsDict["ECal Clusters"] = allClustersArray;
+  eventDict["Jets"] = allJetsDict;
 
-    std::cout << "\n collection:  " << "SimTrackerHits" <<  " of type " <<  sths.getValueTypeName() << "\n\n"
-        << sths << std::endl ;
-
-    int nsh = 5 ;
-    for(int j=0 ; j< nsh ; ++j){
-
-      auto sth1 = sths[2*j] ;
-      if( sth1.getCellID() !=  0xabadcaffee )  throw std::runtime_error("cellID != 0xabadcaffee") ;
-      if( sth1.getEDep() !=  j * 0.000001f ) throw std::runtime_error("e_dep != j * 0.000001") ;
-      if( !( sth1.getPosition() == edm4hep::Vector3d( j*10. , j*20., j*5. )) )
-        throw std::runtime_error("position != ( j*10. , j*20., j*5. )") ;
-      if( !(sth1.getMCParticle() == mcps[6]) )  throw std::runtime_error("mcp != mcps[6]")  ;
-
-      auto sth2 = sths[2*j+1] ;
-      if( sth2.getCellID() !=   0xcaffeebabe )  throw std::runtime_error("cellID != 0xcaffeebabe") ;
-      if( sth2.getEDep() !=  j * 0.001f ) throw std::runtime_error("e_dep != j * 0.001") ;
-      if( !( sth2.getPosition() == edm4hep::Vector3d( -j*10. , -j*20., -j*5. )) )
-        throw std::runtime_error("position != ( -j*10. , -j*20., -j*5. )") ;
-      if( !(sth2.getMCParticle() == mcps[7]) )  throw std::runtime_error("mcp != mcps[7]")  ;
-    }
-
-  } else {
-    throw std::runtime_error("Collection 'SimTrackerHits' should be present");
-  }
-
-  //===============================================================================
-  if( sccons.isValid() ){
-
-    std::cout << "\n collection:  " << "SimCalorimeterHitContributions" <<  " of type "
-        <<  sccons.getValueTypeName() << "\n\n"
-        << sccons << std::endl ;
-
-  } else {
-    throw std::runtime_error("Collection 'SimCalorimeterHitContributions' should be present");
-  }
-  //===============================================================================
-  if( schs.isValid() ){
-
-    std::cout << "\n collection:  " << "SimCalorimeterHit" <<  " of type "
-        <<  schs.getValueTypeName() << "\n\n"
-        << schs << std::endl ;
-
-
-    int nch = 5 ;
-    for(int j=0 ; j< nch ; ++j){
-
-      auto sch1 = schs[2*j] ;
-      if( sch1.getCellID() !=  0xabadcaffee )  throw std::runtime_error("cellID != 0xabadcaffee") ;
-      if( sch1.getEnergy() !=  j*0.1f ) throw std::runtime_error("energy != j*0.1f") ;
-      if( !( sch1.getPosition() == edm4hep::Vector3f( j*100.f , j*200.f, j*50.f )) )
-        throw std::runtime_error("position != ( j*100. , j*200., j*50. )") ;
-
-      auto cont1 = sch1.getContributions(0) ;
-
-      if( !( cont1.getPDG() == 11 )  ||
-          !( cont1.getEnergy() ==  j*0.1f ) ||
-          !( cont1.getTime() ==  j*1e-9f )  ||
-          !( cont1.getStepPosition() == edm4hep::Vector3f( j*100.01f , j*200.01f, j*50.01f ) ) ||
-          !( cont1.getParticle() == mcps[6] )
-  ){
-  throw std::runtime_error("contribution1 does not match ") ;
-      }
-
-      auto sch2 = schs[2*j+1] ;
-      if( sch2.getCellID() !=   0xcaffeebabe )  throw std::runtime_error("cellID != 0xcaffeebabe") ;
-      if( sch2.getEnergy() !=  j*0.2f ) throw std::runtime_error("energy != j*0.2") ;
-      if( !( sch2.getPosition() == edm4hep::Vector3f( -j*100.f , -j*200.f, -j*50.f )) )
-          throw std::runtime_error("position != ( -j*100.f , -j*200.f, -j*50.f )") ;
-
-     auto cont2 = sch2.getContributions(0) ;
-
-      if( !( cont2.getPDG() == -11 )  ||
-          !( cont2.getEnergy() ==  j*0.2f ) ||
-          !( cont2.getTime() ==  j*1e-9f )  ||
-          !( cont2.getStepPosition() == edm4hep::Vector3f( -j*100.01f , -j*200.01f, -j*50.01f ) ) ||
-          !( cont2.getParticle() == mcps[7] )
-        ){
-          throw std::runtime_error("contribution2 does not match ") ;
-      }
-    }
-
-  } else {
-    throw std::runtime_error("Collection 'SimCalorimeterHits' should be present");
-  }
-
- // //===============================================================================
- //  if( sccons.isValid() ){
- //  } else {
- //    throw std::runtime_error("Collection 'SimCalorimeterHitContributions' should be present");
- //  }
-
+  return eventDict;
 }
 
 template<typename ReaderT>
@@ -173,15 +87,24 @@ void read_events(const std::string& filename) {
   auto store = podio::EventStore();
   store.setReader(&reader);
 
+  nlohmann::json allEventsDict;
+
   unsigned nEvents = reader.getEntries();
   for(unsigned i=0; i<nEvents; ++i) {
     if(i%1000==0)
       std::cout<<"reading event "<<i<<std::endl;
-    processEvent(store, true, i);
+    auto eventDict = processEvent(store, true, i);
+    allEventsDict["Event " + std::to_string(i)] = eventDict;
     store.clear();
     reader.endOfEvent();
   }
   reader.closeFile();
+
+  std::ofstream jsonFile("fccee-lar-ecal.json");
+  if (jsonFile.is_open()) {
+    jsonFile << allEventsDict << std::endl;
+    jsonFile.close();
+  }
 }
 
 #endif /* EDM4HEP_TO_JSON_H__ */
--- src/edm4hep2json.cxx
+++ src/edm4hep2json.cxx
@@ -2,7 +2,7 @@
 #include "podio/ROOTReader.h"
 
 int main() {
-  read_events<podio::ROOTReader>("input/fccee-lar-ecal-evnts.root");
+  read_events<podio::ROOTReader>("input/fccee-lar-ecal-envts.root");
 
   return EXIT_SUCCESS;
 }
